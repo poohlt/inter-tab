0e08ff1628e613e4df03e94d063f806fa95bc168
Tianyu Liu <poohlty@sina.com>
2014-04-26 13:50:22 -0400

    Add simple test with zuul

---
+++ test/test.js
@@ -0,0 +1,10 @@
+// var url = require('../lib/url');
+var expect = require('expect.js');
+
+describe('inter-tab', function(){
+
+  it('should run simple test', function(){
+    expect(1).to.be(1);
+  });
+
+});
