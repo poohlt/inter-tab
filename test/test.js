daedbb96d53c4929fa929524b97829863beefa90
Tianyu Liu <poohlty@sina.com>
2014-04-27 15:40:43 -0400

    Add tests for getTabs

--- test/test.js
+++ test/test.js
@@ -7,4 +7,23 @@ describe('inter-tab', function(){
         expect(tab.id).to.be.a('number');
         tab.destroy();
     });
+
+    it('should yield same tabs array', function(){
+        var tab1 = new Manager();
+        var tab2 = new Manager();
+        var tabIds1 = tab1.getTabs();
+        var tabIds2 = tab2.getTabs();
+        expect(tabIds1).to.eql(tabIds2);
+        tab1.destroy();
+        tab2.destroy();
+    });
+
+    it('should contain right keys in tabs array', function(){
+        var tab1 = new Manager();
+        var tab2 = new Manager();
+        var tabIds = tab1.getTabs();
+        expect(tabIds).to.only.have.keys(tab1.id.toString(), tab2.id.toString());
+        tab1.destroy();
+        tab2.destroy();
+    });
 });
