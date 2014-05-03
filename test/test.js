fc8a13958015ae16c24ee7d6189e53a1a4c7b417
Tianyu Liu <poohlty@sina.com>
2014-05-02 23:26:01 -0400

    Add test for destroy and set/get

--- test/test.js
+++ test/test.js
@@ -26,4 +26,23 @@ describe('inter-tab', function(){
         tab1.destroy();
         tab2.destroy();
     });
+
+    it('should correctly destroy a tab', function(){
+        var tab1 = new Manager();
+        var tab2 = new Manager();
+        tab1.destroy();
+        var tabIds = tab2.getTabs();
+        expect(tabIds).to.only.have.keys(tab2.id.toString());
+        tab2.destroy();
+    });
+
+    it('should correctly set key-value pairs', function(){
+        var tab1 = new Manager();
+        var tab2 = new Manager();
+        tab2.set(tab1.id, 'test', 'Hello World');
+        var attr = tab2.get(tab1.id, 'test');
+        expect(attr).to.be('Hello World');
+        tab1.destroy();
+        tab2.destroy();
+    });
 });
